58b3c6b2b707d55d47458c7668babdf7cc744a74
javohir428 <ultracers200@gmail.com>
2019-12-07 05:19:29 +0300

    Initial commit

---
+++ app/src/main/java/com/javosoft/burgeron/SplashScreenActivity.java
@@ -0,0 +1,23 @@
+package com.javosoft.burgeron;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+    }
+}
